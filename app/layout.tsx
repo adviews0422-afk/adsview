01093902b9ea7425a82a83d5b40c198a9591e6e4
adviews0422-afk <adviews0422@gmail.com>
2026-05-14 21:36:28 +0800

    git push

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <head>
-        <meta name='google-adsense-account' content='ca-pub-7785908484017299' />
-      </head>
       <body className={inter.className}>
         <StoreProvider>
           <AuthProvider>
